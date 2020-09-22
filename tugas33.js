444f2f5a752b3183b47e6afe91070b4c17fe9d6b
yanto-sugiyanto <71365660+yanto-sugiyanto@users.noreply.github.com>
2020-09-22 11:17:22 +0700

    Add files via upload

---
+++ tugas33.js
@@ -0,0 +1,11 @@
+function nama(){
+var selector1 = document.queryselector("#firstname")
+var selector2 = document.queryselector(".forminput")
+var selector3 = document.queryselector("input")
+var selectorAll = document.queryselectorAll("input")
+
+console.log (selector1)
+console.log (selector2)
+console.log (selector3)
+
+}
